6e4f4262318d7e1549aa31b1f6c60a3b51b18472
woragis <masteringthecode.woragis+github@gmail.com>
2024-11-19 16:48:42 -0300

    mend

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 mod db;
-use db::init_db;
+mod routes;
+
+use actix_web::{web, App, HttpServer};
+use db::connection::connect_db;
+use routes::user_routes::config_user_routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    init_db().await;
-    println!("Program is running!");
+    // Establish database connection
+    let client = connect_db()
+        .await
+        .expect("Failed to connect to the database");
 
-    Ok(())
+    // Start Actix Web server
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(client.clone())) // Share DB connection
+            .configure(config_user_routes) // Configure user routes
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
